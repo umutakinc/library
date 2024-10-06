5842f0a2331483326667e3fdfa74263a3ab23f04
umutakinc <umutakinci5@gmail.com>
2024-10-06 14:36:10 +0300

    dev completed

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const ADD_BOOK_BTN = document.querySelector('#addBookBtn');
 const SUBMIT_BTN = document.querySelector('#submitBtn');
 const CLOSE_BTN = document.querySelector('#closeBtn');
 const FORM = document.querySelector('#toggleForm');
+const myLibrary = [];
 
 function showForm() {
     FORM.classList.remove('d-none');
@@ -15,8 +16,44 @@ function hideForm() {
     });
 }
 
-ADD_BOOK_BTN.addEventListener('click', showForm);   
-SUBMIT_BTN.addEventListener('click', hideForm);
-CLOSE_BTN.addEventListener('click', hideForm);
+function getFormValues(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
 
+    return formProps
+}
+
+function Book(bookName, bookAuthor, bookPages, bookIsRead) {
+    this.bookName = bookName;
+    this.bookAuthor = bookAuthor;
+    this.bookPages = bookPages;
+    this.bookIsRead = bookIsRead;
+}
+  
+function addBookToLibrary(e) {
+    const bookInfos = getFormValues(e);
+    const book = new Book(bookInfos.bookName, bookInfos.bookAuthor, bookInfos.bookPages, bookInfos.bookIsRead)
+    myLibrary.push(book);
+}
+
+function printBooksScreen(e) {
+    addBookToLibrary(e);
+    let arrayLastEl = myLibrary[myLibrary.length - 1];
+
+    document.querySelector('.book-container').innerHTML += `
+        <div class="book">
+            <h2 class="book-title">${arrayLastEl.bookName}</h2>
+            <div>
+                <h4 class="book-author">Yazar: <span>${arrayLastEl.bookAuthor}</span></h4>
+                <h4 class="book-pages">Sayfa Sayısı: <span>${arrayLastEl.bookPages}</span></h4>
+                <h4 class="book-isRead">${arrayLastEl.bookIsRead ? 'Okundu' : 'Okunmadı'}</h4>
+            </div>
+        </div>
+    `;
+}
+
+ADD_BOOK_BTN.addEventListener('click', showForm);
+CLOSE_BTN.addEventListener('click', hideForm);
+FORM.addEventListener('submit', printBooksScreen);
 
